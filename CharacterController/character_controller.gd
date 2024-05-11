5d3268ab025b5066d88d3c0cfdb4fc6b1e9d8859
Saaswath Kumar <saaskum12@gmail.com>
2024-05-11 00:08:26 -0400

    Added basic movement

--- CharacterController/character_controller.gd
+++ CharacterController/character_controller.gd
@@ -1,6 +1,22 @@
 extends CharacterBody3D
 
 @export var look_sens: float = 0.006
+@export var jump_velocity := 6.0
+@export var bhop_on := true
+@export var walk_speed := 7.0
+
+const HEADBOB_SWAY_AMOUNT = 0.05
+const HEADBOB_FREQ = 2.5
+var headbob_time = 0.0
+
+@export var air_cap := 0.85
+@export var air_accel := 880.0
+@export var air_move_speed := 500.0
+
+var wish_dir := Vector3.ZERO
+
+func get_move_speed() -> float:
+	return walk_speed
 
 func _ready():
 	for child in %WorldModel.find_children("*", "VisualInstance3D"):
@@ -21,9 +37,45 @@ func _unhandled_input(event):
 		
 		
 		
-
-func _physics_process(delta):
-	pass
-	
+func _headbob_effect(delta):
+	headbob_time += delta * self.velocity.length()
+	%Camera3D.transform.origin * Vector3(
+		cos(headbob_time * HEADBOB_FREQ * 0.5) * HEADBOB_SWAY_AMOUNT,
+		sin(headbob_time * HEADBOB_FREQ) * HEADBOB_SWAY_AMOUNT,
+		0,
+	)
 func _process(delta):
 	pass
+
+func _handle_air_physics(delta) -> void:
+	self.velocity.y -= ProjectSettings.get_setting("physics/3d/default_gravity") * delta
+	
+	var cur_speed_in_wish_dir = self.velocity.dot(wish_dir)
+	
+	var max_speed = min((air_move_speed * wish_dir).length(), air_cap)
+	
+	var add_speed_till_max = max_speed - cur_speed_in_wish_dir
+	if add_speed_till_max > 0:
+		var accel_speed = air_accel * air_move_speed * delta
+		accel_speed = min(accel_speed, add_speed_till_max)
+		self.velocity += accel_speed * wish_dir
+	
+	
+func _handle_ground_physics(delta) -> void:
+	self.velocity.x = wish_dir.x * get_move_speed()
+	self.velocity.z = wish_dir.z * get_move_speed()
+	_headbob_effect(delta)
+	
+func _physics_process(delta):
+	var input_dir = Input.get_vector("left", "right", "up", "down").normalized()
+	wish_dir = self.global_transform.basis * Vector3(input_dir.x, 0., input_dir.y)
+	
+	if is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			self.velocity.y = jump_velocity
+		_handle_ground_physics(delta)
+	else:
+		_handle_air_physics(delta)
+	
+	move_and_slide()
+	
